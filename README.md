cd2d8d7d1165d5b4ede64ad0575a6d93b364ca87
Shisir1 <shisirhumagain0@gmail.com>
2022-03-10 23:37:05 -0500

    Dynamic movement added

---
+++ README.md
@@ -0,0 +1,1 @@
+#JavaScript Money Manger
--- app.js
+++ app.js
@@ -22,3 +22,70 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+const accountOne = {
+  owner: 'Gorakh Raj Joshi',
+  movements: [200,-300, 400,500,-700,1300,-1100,1600
+  //   function add(){
+  //   return 'Hello World!';
+  // }
+],
+  interestRate: 1.5,
+  pin: 1234,
+};
+
+const accountTwo = {
+  owner: 'Shisir Humagain',
+  movements: [1000,-600,700,-300,1300,-1100,1600],
+  interestRate: 1.1,
+  pin: 2222,
+};
+
+const accounts = [accountOne,accountTwo];
+
+// console.log(accounts);
+
+const [{movements}] = accounts;
+// console.log(movements);
+
+//forEach
+
+//must pass a function as an argument for 'forEach' and it takes 3 parameters, 1st one is actual element, 2nd one is Index and 3rd one is Array itself!
+// movements.forEach(function(movement,i,array){
+//   console.log(`Index ${i} of value ${movement}`);
+// });
+
+// const [,{movements:movementTwo}] = accounts;
+// console.log(movementTwo);
+
+// movementTwo.forEach(function(movement,i,array){
+//   console.log(movement);
+// })
+
+// < 0 => withdrawal 
+// > 0 => deposit
+
+movements.forEach(function(mov,i){
+  // if(mov < 0) {
+  //   console.log('Withdrawal');
+  // }else {
+  //   console.log('Deposit');
+  // }
+  // console.log(mov > 0 ? 'Deposite':'Withdrawal');
+  const type = mov > 0 ? 'deposit':'withdrawal';
+  console.log(type);
+
+  const html = `<div class="movements__row">
+  <div class="movements__type movements__type--${type}">
+    ${i + 1} ${type}
+  </div>
+  <div class="movements__value">${mov}€</div>
+</div>`;
+containerMovements.insertAdjacentHTML('afterbegin',html);
+});
+console.log(containerMovements);
+
+
+
+
+
